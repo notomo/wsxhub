02df988b152d4f0ece1553f14fb780409e4d367c
notomo <notomo.motono@gmail.com>
2019-06-27 08:45:27 +0900

    Add error exit code and output test

--- test/ping_test.go
+++ test/ping_test.go
@@ -1,14 +1,40 @@
 package command_test
 
 import (
+	"bufio"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestPingFailure(t *testing.T) {
-	err := exec.Command("../dist/wsxhub", "--port", insidePort, "ping").Wait()
-	if err == nil {
+	cmd := exec.Command("../dist/wsxhub", "--port", insidePort, "ping")
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	received := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			received <- scanner.Text()
+			break
+		}
+	}()
+
+	if err := cmd.Run(); err == nil {
 		t.Fatal("`wsxhub ping` must fail if `wsxhub server` is not executed.")
+	}
+	exitCode := cmd.ProcessState.ExitCode()
+	if exitCode != 1 {
+		t.Fatalf("The exit code must be 1, but actual: %d", exitCode)
+	}
+
+	select {
+	case <-received:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("stderr output not found")
 	}
 }
 
